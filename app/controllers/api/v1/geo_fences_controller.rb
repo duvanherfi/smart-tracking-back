e43fb72af6a073084a4f97d6696bc1ab6b61ed3b
Duvan Hernandez Figueroa <duvanherfi@gmail.com>
2025-05-07 21:58:00 -0500

    SCRUM-82 SCRUM-88 SCRUM-132 SCRUM-95 Fix params for geofences controller

--- app/controllers/api/v1/geo_fences_controller.rb
+++ app/controllers/api/v1/geo_fences_controller.rb
@@ -48,7 +48,7 @@ class Api::V1::GeoFencesController < ApiApplicationController
 
   def geo_fence_params
     params.require(:geo_fence).permit(
-      :name, :description, :area_geojson, :centroid_geojson, :is_enabled, vehicle_ids: []
+      :name, :description, :radius, :is_enabled, vehicle_ids: [],  area_geojson: {}, centroid_geojson: {}
     )
   end
 end
